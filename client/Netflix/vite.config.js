abc902a23edc4fa36d97fb220d69ec2d16369ae9
Shaik Afreen10 <afshaik775@gmail.com>
2025-09-11 21:23:36 +0530

    Update movie manager with edit functionality

--- client/Netflix/vite.config.js
+++ client/Netflix/vite.config.js
@@ -1,9 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),
+//          tailwindcss(),
+//   ],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-         tailwindcss(),
+  base: '/Netflex/',  // <-- This is super important for GitHub Pages
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
-})
+});
